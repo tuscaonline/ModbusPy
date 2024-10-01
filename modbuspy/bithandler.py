fda60c08b2999bedb37692f83dacd56ea7e25352
Pierre Nerriere <pierre@nerriere.net>
2024-10-01 20:48:58 +0200

    bool_to_int

--- modbuspy/bithandler.py
+++ modbuspy/bithandler.py
@@ -1,7 +1,15 @@
 from functools import reduce
+from itertools import batched
+import struct
 
 def f(a, b):
     return (a << 1) | b
 
-def bools_to_int(arr):
-    return reduce(f, arr)
+def bools_to_int(arr:list[bool])->bytearray:
+    ret = b""
+    for i in batched(reversed(arr), 16):
+        octet = 0
+        for rang ,j in enumerate(i):
+            octet |=  j << rang
+        ret += struct.pack(">B", octet)
+    return ret
--- tests/test_bithandler.py
+++ tests/test_bithandler.py
@@ -1,7 +1,14 @@
 from modbuspy import bithandler
+import bitarray
 
+
+bits= [ True, True, False, False, False, True, False, True]
 
 def test_packbit():
-    test = bithandler.bools_to_int([ True, False, False, False, False, True, True, True])
-    assert test == b"0\x87"
+    test = bithandler.bools_to_int(bits)
+    assert test == b"\xc5"
     pass
+
+def test_bitsarray():
+    a= bitarray.bitarray( bits)
+    assert a
